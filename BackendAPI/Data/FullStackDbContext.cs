d2040cde2fac0e4fa2d18c30a142514f9eb8a2bd
Altaf Ghori <altafhusenghori.ag@gmail.com>
2023-04-07 14:25:04 +0530

    first commit

---
+++ BackendAPI/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BackendAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendAPI.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options) 
+        { 
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
